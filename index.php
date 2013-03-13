ff9d8c841fbd0e5dd18305553651ac287d0743dc
septian-bhoechie <septian.bhoechie@gmail.com>
2013-03-13 22:14:49 +0700

    last update

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	define('ENVIRONMENT', 'development');
 /*
  *---------------------------------------------------------------
- * ERROR REPORTING///
+ * ERROR REPORTING
  *---------------------------------------------------------------
  *
  * Different environments will require different levels of error reporting.
